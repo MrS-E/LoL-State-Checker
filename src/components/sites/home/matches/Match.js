8971308933ba1feca3636c5335f3efc4c561ca3a
MrS-E <simeon.stix@gmx.ch>
2022-11-02 17:47:48 +0100

    Initial commit

--- src/components/sites/home/matches/Match.js
+++ src/components/sites/home/matches/Match.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {get_request} from "../../../other/links";
 import useFetch from "../../../hooks/useFetch";
 import '../css/Match.css'
-import Summoner from "./Summoner";
+import {summoner_icon} from "../../../other/links";
 
 const Match = (props) => {
     const url = get_request("match_by_id",props.region,"region",[props.id],"query");
@@ -13,9 +13,9 @@ const Match = (props) => {
         let summoner_red=[];
         for(let x=0;x<data.info.participants.length;x++){
             if(x<5){
-                summoner_blue.push(data["info"]["participants"][x]["summonerName"]);
+                summoner_blue.push([data["info"]["participants"][x]["summonerName"],data["info"]["participants"][x]["profileIcon"]]);
             }else if(x<10){
-                summoner_red.push(data["info"]["participants"][x]["summonerName"]);
+                summoner_red.push([data["info"]["participants"][x]["summonerName"],data["info"]["participants"][x]["profileIcon"]]);
             }
         }
         let className = "c100";
@@ -24,14 +24,16 @@ const Match = (props) => {
         }
         return (
             <div className={className + " games"} id={props.id}>
-                <h6>{data.info.gameMode}</h6>
-                {props.id}
+                <div>
+                    <h6>{data.info.gameMode}</h6>
+                    {props.id}
+                </div>
                 <div className="summoner">
                     <div className="sum_blue">
-                        {summoner_blue.map((x)=>{return( <p className="sum">{x}</p>)})}
+                        {summoner_blue.map((x)=>{return(<p className="sum"><img src={summoner_icon+x[1]+".png"} className="rounded-circle icon img" alt={x[1]}/>{x[0]}</p>)})}
                     </div>
                     <div className="sum_red">
-                        {summoner_red.map((x)=>{return(<Summoner name={x}/>)})}
+                        {summoner_red.map((x)=>{return(<p className="sum"><img src={summoner_icon+x[1]+".png"} className="rounded-circle icon img" alt={x[1]}/>{x[0]}</p>)})}
                     </div>
                 </div>
             </div>
