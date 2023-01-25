ee7856f49892eb98095d9cca56c9d9cdc850a36d
MrS-E <simeon.stix@gmx.ch>
2023-01-25 21:32:19 +0100

    small fix to navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import Champions from "./components/sites/champions/Champions";
 import Home from "./components/sites/home/Home";
 import Items from "./components/sites/items/Items";
-import {Nav, Navbar, NavLink} from "react-bootstrap";
+import {Container, Nav, Navbar, NavLink} from "react-bootstrap";
 import Quiz from "./components/sites/quitz/Quiz";
 
 function App() {
@@ -11,15 +11,17 @@ function App() {
     <>
         <BrowserRouter>
                 <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-                    <Navbar.Toggle aria-controls="navbarScrools" data-bs-target="#navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav>
-                            <NavLink eventKey="1" as={Link} to="/">Home</NavLink>
-                            <NavLink eventKey="2" as={Link} to="/champions">Champions</NavLink>
-                            <NavLink eventKey="3" as={Link} to="/items">Items</NavLink>
-                            <NavLink eventKey="4" as={Link} to="/quiz">Quiz</NavLink>
-                        </Nav>
-                    </Navbar.Collapse>
+                    <Container fluid>
+                        <Navbar.Toggle aria-controls="navbarScrools" data-bs-target="#navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav>
+                                <NavLink eventKey="1" as={Link} to="/">Home</NavLink>
+                                <NavLink eventKey="2" as={Link} to="/champions">Champions</NavLink>
+                                <NavLink eventKey="3" as={Link} to="/items">Items</NavLink>
+                                <NavLink eventKey="4" as={Link} to="/quiz">Quiz</NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
                 </Navbar>
                 <div className="container">
                     <Switch>
